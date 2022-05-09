61a0b8efe2f99e60dddad416d575f86180df3383
LinhLe2307 <lemylinh23071997@gmail.com>
2022-05-09 16:40:22 +0300

    Add axios post

--- src/components/AddRecipes.js
+++ src/components/AddRecipes.js
@@ -1,35 +1,75 @@
-import React from 'react'
+import { React, useState } from 'react';
+import axios from 'axios';
 
 const AddRecipes = () => {
+  const [recipes, setRecipes] = useState({
+    name: "",
+    author: "",
+    country: "",
+    description: "",
+    image: "",
+    instructions: ""
+  });
+  const [ingredients, setIngredients] = useState({
+    quantity: "",
+    ingredient: ""
+  })
+
+  const inputHandler = (e) => {
+    setRecipes(() => ({
+      ...recipes,
+      [e.target.name]: e.target.value
+    }));
+    setIngredients(() => ({
+      ...ingredients,
+      [e.target.name]: e.target.value,
+    }));
+  }
+  const addSubmitHandler = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3010/notes", recipes, ingredients)
+      .then(res => console.log("success", res))
+      .catch(error => console.log("error", error))
+  }
+
   return (
     <div>
       <h1>Add New Recipe</h1>
-      <div>
-        <label>Name</label>
-        <input type="text" />
-      </div>
-      <div>
-        <label>Author</label>
-        <input type="text" />
-      </div>
-      <div>
-        <label>Recipe is from</label>
-        <input type="text" />
-      </div>
-      <div>
-        <label>Description</label>
-        <textarea type="text" />
-      </div>
-      <div>
-        <label>Image</label>
-        <input type="text" />
-      </div>
-      <div>
-        <label>Ingredient</label>
-        <label>Quantity</label>
-        <input type="text" />
-      </div>
-
+      <form onChange={inputHandler} onSubmit={addSubmitHandler}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" />
+        </div>
+        <div>
+          <label htmlFor="author">Author</label>
+          <input type="text" id="author" name="author" />
+        </div>
+        <div>
+          <label htmlFor="country">Recipe is from</label>
+          <input type="text" id="country" name="country" />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <textarea type="text" id="description" name="description" />
+        </div>
+        <div>
+          <label htmlFor="image">Image</label>
+          <input type="text" id="image" name="image" />
+        </div>
+        <div>
+          <label>Ingredients</label>
+          <label htmlFor="quantity">Quantity</label>
+          <input type="text" id="quantity" name="quantity" />
+          <label htmlFor="ingredient">Ingredient</label>
+          <input type="text" id="ingredient" name="ingredient" />
+        </div>
+        <div>
+          <label htmlFor="instructions">Instructions</label>
+          <textarea id="instructions" name="instructions" />
+        </div>
+        <button type="submit">SUBMIT</button>
+      </form>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header>TasteIt</header>
+    <header>
+      <h2><Link to="/">TasteIT</Link></h2>
+    </header>
   )
 }
 
